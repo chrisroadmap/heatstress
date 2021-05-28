5115f458c69690c90ec9094bc51bf5e3012fab84
Chris Smith <chrisroadmap@gmail.com>
2021-05-28 14:41:51 +0100

    add regridder for CMIP UTCI data

--- jasmin-scripts/regrid_models.py
+++ jasmin-scripts/regrid_models.py
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 #!/usr/bin/env python
 
+=======
+>>>>>>> add regridder for CMIP UTCI data
 """
 This script regrids the climate model output data to 1x1 degree.
 """
@@ -12,7 +15,10 @@ import iris.coord_categorisation
 import numpy as np
 import glob
 from climateforcing.utils import mkdir_p
+<<<<<<< HEAD
 import sys
+=======
+>>>>>>> add regridder for CMIP UTCI data
 
 # Make a dummy grid
 latitude = iris.coords.DimCoord(
@@ -45,6 +51,7 @@ dummy_cube.coord('latitude').guess_bounds()
 origdir = '/gws/pw/j05/cop26_hackathons/bristol/project10/utci_projections/'
 regriddir = '/gws/pw/j05/cop26_hackathons/bristol/project10/utci_projections_1deg/'
 
+<<<<<<< HEAD
 models = ['HadGEM3-GC31-LL', 'BCC-CSM2-MR']
 runs = {
     'HadGEM3-GC31-LL': ['r1i1p1f3'],
@@ -61,6 +68,8 @@ for model in models:
         first_file=True
         for scenario in scenarios[model][run]:
             filelist = glob.glob(origdir + '%s/%s/%s/*.nc' % (model, scenario, run))
+=======
+>>>>>>> add regridder for CMIP UTCI data
 models = ['HadGEM3-GC31-LL']
 runs = {
     'HadGEM3-GC31-LL': ['r1i1p1f3']
