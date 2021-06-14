0e906ac9c10d3f9a05b5fc8792eaf7280e1bb940
Chris Smith <chrisroadmap@gmail.com>
2021-06-14 19:12:46 +0100

    regrid remaining models

--- jasmin-scripts/regrid_models.py
+++ jasmin-scripts/regrid_models.py
@@ -44,7 +44,7 @@ dummy_cube.coord('latitude').guess_bounds()
 # do the regrid and save output
 #origdir = '/gws/pw/j05/cop26_hackathons/bristol/project10/utci_projections/'
 origdir = '/work/scratch-nopw/pmcjs/'
-regriddir = '/gws/pw/j05/cop26_hackathons/bristol/project10/utci_projections_1deg/'
+regriddir = '/work/scratch-nopw/pmcjs/utci_projections_1deg/'
 
 #models = ['HadGEM3-GC31-LL', 'BCC-CSM2-MR', 'CMCC-ESM2']
 #runs = {
@@ -65,27 +65,21 @@ regriddir = '/gws/pw/j05/cop26_hackathons/bristol/project10/utci_projections_1de
 #    'CMCC-ESM2': ['r1i1p1f1']
 #}
 scenarios = {}
-scenarios['CMCC-ESM2'] = {}
-scenarios['CMCC-ESM2']['r1i1p1f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
+scenarios['ACCESS-CM2'] = {}
+scenarios['ACCESS-CM2']['r1i1p1f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
+scenarios['CanESM5'] = {}
+scenarios['CanESM5']['r1i1p2f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
+scenarios['CMCC-CM2-SR5'] = {}
+scenarios['CMCC-CM2-SR5']['r1i1p1f1'] = ['historical', 'ssp245', 'ssp585']
+scenarios['MRI-ESM2-0'] = {}
+scenarios['MRI-ESM2-0']['r1i1p1f1'] = ['historical', 'ssp585']
+scenarios['KACE-1-0-G'] = {}
+scenarios['KACE-1-0-G']['r1i1p1f1'] = ['ssp245']
+scenarios['HadGEM3-GC31-MM'] = {}
+scenarios['HadGEM3-GC31-MM']['r1i1p1f3'] = ['historical', 'ssp126', 'ssp585']
 
 for model in scenarios:
     for run in scenarios[model]:
-        first_file=True
-        for scenario in scenarios[model][run]:
-            filelist = glob.glob(origdir + '%s/%s/%s/*.nc' % (model, scenario, run))
-models = ['HadGEM3-GC31-LL', 'BCC-CSM2-MR']
-runs = {
-    'HadGEM3-GC31-LL': ['r1i1p1f3'],
-    'BCC-CSM2-MR': ['r1i1p1f1'],
-}
-scenarios = {}
-scenarios['HadGEM3-GC31-LL'] = {}
-scenarios['BCC-CSM2-MR'] = {}
-scenarios['HadGEM3-GC31-LL']['r1i1p1f3'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
-scenarios['BCC-CSM2-MR']['r1i1p1f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
-
-for model in models:
-    for run in runs[model]:
         first_file=True
         for scenario in scenarios[model][run]:
             filelist = glob.glob(origdir + '%s/%s/%s/*.nc' % (model, scenario, run))
