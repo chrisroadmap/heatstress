b6567b8da909a349bf00c5ffe3bf6da86006fefa
Chris Smith <chrisroadmap@gmail.com>
2021-06-17 10:03:12 +0100

    extract percentiles from regridded models

--- jasmin-scripts/extract_stats.py
+++ jasmin-scripts/extract_stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-This script calculates the annual percentiles for ERA5-HEAT and HadGEM3 historical
+This script calculates the annual percentiles for all models and scenarios
 """
 
 import iris
@@ -13,23 +13,46 @@ import matplotlib.pyplot as pl
 from climateforcing.utils import mkdir_p
 import numpy as np
 import scipy.stats as st
-from tqdm import tqdm
+import sys
 
-#era5heatdir = '/gws/pw/j05/cop26_hackathons/bristol/project10/era5-heat_1deg/'
-#outdir = '/gws/pw/j05/cop26_hackathons/bristol/project10/era5-heat_1deg_percentiles/'
-#mkdir_p(outdir)
-#
-#for year in tqdm(range(1985, 2015)):
-#    cube_era5 = iris.load_cube(era5heatdir + 'ECMWF_utci_%4d_v1.0_con.nc' % year)
-#    result = cube_era5.collapsed('time', iris.analysis.PERCENTILE, percent=[95, 98, 99, 99.5, 99.9, 100])
-#    iris.save(result, outdir + 'ECMWF_utci_%4d_v1.0_con.nc' % year)
-#
-modeldir = '/work/scratch-nopw/pmcjs/utci_projections_1deg/HadGEM3-GC31-LL/historical/r1i1p1f3/'
-outdir = '/gws/pw/j05/cop26_hackathons/bristol/project10/utci_projections_1deg_percentiles/HadGEM3-GC31-LL/historical/r1i1p1f3/'
+scenarios = {}
+scenarios['ACCESS-CM2'] = {}
+scenarios['ACCESS-CM2']['r1i1p1f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
+scenarios['CanESM5'] = {}
+scenarios['CanESM5']['r1i1p2f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
+scenarios['CMCC-CM2-SR5'] = {}
+scenarios['CMCC-CM2-SR5']['r1i1p1f1'] = ['historical', 'ssp245', 'ssp585']
+scenarios['MRI-ESM2-0'] = {}
+scenarios['MRI-ESM2-0']['r1i1p1f1'] = ['historical', 'ssp585']
+scenarios['KACE-1-0-G'] = {}
+scenarios['KACE-1-0-G']['r1i1p1f1'] = ['ssp245']
+scenarios['HadGEM3-GC31-MM'] = {}
+scenarios['HadGEM3-GC31-MM']['r1i1p1f3'] = ['historical', 'ssp126', 'ssp585']
+scenarios['HadGEM3-GC31-LL'] = {}
+scenarios['HadGEM3-GC31-LL']['r1i1p1f3'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
+scenarios['BCC-CSM2-MR'] = {}
+scenarios['BCC-CSM2-MR']['r1i1p1f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
+scenarios['CMCC-ESM2'] = {}
+scenarios['CMCC-ESM2']['r1i1p1f1'] = ['historical', 'ssp126', 'ssp245', 'ssp585']
 
-mkdir_p(outdir)
-for year in tqdm(range(1985, 2015)):
-    cube_model = iris.load_cube(modeldir + 'utci_3hr_HadGEM3-GC31-LL_historical_r1i1p1f3_gn_%4d01010300-%4d01010000.nc' % (year, year+1))
-    cube_model.coord('time').points = cube_model.coord('time').points - 1/16  # put on radiation timesteps
-    result = cube_model.collapsed('time', iris.analysis.PERCENTILE, percent=[95, 98, 99, 99.5, 99.9, 100])
-    iris.save(result, outdir + 'utci_3hr_HadGEM3-GC31-LL_historical_r1i1p1f3_gn_%4d.nc' % year)
+for model in scenarios:
+    for run in scenarios[model]:
+        for scenario in scenarios[model][run]:
+            modeldir = '/work/scratch-nopw/pmcjs/utci_projections_1deg/%s/%s/%s/' % (model, scenario, run)
+            outdir = '/gws/pw/j05/cop26_hackathons/bristol/project10/utci_projections_1deg_percentiles/%s/%s/%s/' % (model, scenario, run)
+            mkdir_p(outdir)
+            if scenario == 'historical':
+                firstyear = 1985
+                lastyear = 2015
+            else:
+                firstyear = 2015
+                lastyear = 2101
+            for year in range(firstyear, lastyear):
+                cube_model = iris.load_cube(modeldir + 'utci_3hr_%s_%s_%s_gn_%4d01010300-%4d01010000.nc' % (model, scenario, run, year, (year+1)))
+                if model in ['BCC-CSM2-MR', 'MRI-ESM2-0']:
+                    cube_model.coord('time').points = cube_model.coord('time').points + 1/16
+                else:
+                    cube_model.coord('time').points = cube_model.coord('time').points - 1/16  # put on radiation timesteps
+                result = cube_model.collapsed('time', iris.analysis.PERCENTILE, percent=[95, 98, 99, 99.5, 99.9, 100])
+                iris.save(result, outdir + 'utci_3hr_%s_%s_%s_gn_%4d.nc' % (model, scenario, run, year))
+                sys.stdout.write('%s %s %s %s success' % (model, run, scenario, year))
--- jasmin-scripts/run_extract.sh
+++ jasmin-scripts/run_extract.sh
@@ -2,8 +2,8 @@
 #SBATCH -p short-serial
 #SBATCH -o job_output/%j.out
 #SBATCH -e job_output/%j.err
-#SBATCH -t 03:00:00
-#SBATCH --mem=32000  # 64M needed for HadGEM3-MM, else 32M usually sufficient
+#SBATCH -t 12:00:00
+#SBATCH --mem=16000  # one degree yearly, this will be more than sufficient?
 
 conda activate heatstress
 ./extract_stats.py
