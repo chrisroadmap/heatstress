ff12651eff3bb3b759325863def3ba027e6ba4ca
Chris Smith <chrisroadmap@gmail.com>
2021-06-23 10:11:11 +0100

    setup to run KACE historical off JASMIN (will probably fail)

--- jasmin-scripts/run_batch.sh
+++ jasmin-scripts/run_batch.sh
@@ -2,9 +2,9 @@
 #SBATCH -p short-serial
 #SBATCH -o job_output/%j.out
 #SBATCH -e job_output/%j.err
-#SBATCH -t 24:00:00
-#SBATCH --mem=64000  # 64M needed for HadGEM3-MM, else 32M usually sufficient
-#SBATCH --array=0-45
+#SBATCH -t 06:00:00
+#SBATCH --mem=32000  # 64M needed for HadGEM3-MM, else 32M usually sufficient
+#SBATCH --array=45
 
 conda activate heatstress
 ./run_utci.py ${SLURM_ARRAY_TASK_ID}
