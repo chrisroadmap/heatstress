126c8446bef547923c9dca425447907fc833972d
Chris Smith <chrisroadmap@gmail.com>
2021-06-28 06:23:00 +0100

    set up to run batch script for monthly mean UTCI

---
+++ jasmin-scripts/run_means.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p short-serial
+#SBATCH -o job_output/%j.out
+#SBATCH -e job_output/%j.err
+#SBATCH -t 12:00:00
+#SBATCH --mem=16000  # one degree yearly, this will be more than sufficient?
+
+conda activate heatstress
+./extract_means.py
